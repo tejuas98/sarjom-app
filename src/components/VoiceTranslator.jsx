9fdbe2feb92816b9ac170c44290795f396cf2bc3
Tejas <tejusj98@gmail.com>
2026-09-19 23:09:59 +0530

    fix: import MessageSquare icon in VoiceTranslator

--- src/components/VoiceTranslator.jsx
+++ src/components/VoiceTranslator.jsx
@@ -14,6 +14,7 @@ import {
   Wrench,
   HardDrive,
   SlidersHorizontal,
+  MessageSquare,
 } from 'lucide-react';
 import { translateHindiToTribal, translateTribalToHindi } from '../services/nlpTranslationEngine';
 import { voiceService } from '../services/voiceTranslationService';
